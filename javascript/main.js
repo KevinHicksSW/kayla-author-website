2f9bd64953b3cb14f6e7a8289beee6dbde5fb3ff
Kevin Hicks <kevin@infinia.io>
2017-05-15 20:55:05 -0400

    Add pre-order and news modal

--- javascript/main.js
+++ javascript/main.js
@@ -17,4 +17,28 @@ $('document').ready(function() {
         $(this).addClass('animated fadeOut');
         $('ul.pre-order').addClass('fadeIn');
     });
+
+    $('.pre-order-action-button').click(function(event) {
+        event.preventDefault();
+        $('#pre-order-modal').css('display', 'block');
+        $('#pre-order-modal').removeClass('fadeOut').addClass('fadeIn');
+    });
+
+    $('.sneak-peek-link').click(function(event) {
+        event.preventDefault();
+        $('#news-modal').css('display', 'block');
+        $('#news-modal').removeClass('fadeOut').addClass('fadeIn');
+    });
+
+    $(window).click(function(event) {
+        if ($(event.target).hasClass('modal')) {
+            $('.modal').removeClass('fadeIn').addClass('fadeOut');
+            setTimeout(function() { $('.modal').css('display', 'none'); }, 1000);
+        }
+    });
+
+    $('.close').click(function(event) {
+        $('.modal').removeClass('fadeIn').addClass('fadeOut');
+        setTimeout(function() { $('.modal').css('display', 'none'); }, 1000);
+    });
 });
